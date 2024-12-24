134b67c109220cec351a803818339aee589d3bda
NastyaSia04 <nastyasia04@gmail.com>
2024-12-24 17:06:23 +0200

    edited gendiff.js: added option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@
 import { program } from "commander";
 
 program
+  .helpOption('-h, --help', 'output usage information')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
