af1115aa848a343c315ab58cf898f5eb90e408f9
NastyaSia04 <nastyasia04@gmail.com>
2024-12-26 12:07:18 +0200

    edited bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import gendiff from "../index.js";
+
+gendiff('__fixtures__/file1.json', '__fixtures__/file2.json');
+
+import { Command } from "commander";
+const program = new Command();
 
 program
   .helpOption('-h, --help', 'output usage information')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
+  .arguments('[filepath1] [filepath2]')
   .option('-f, --format [type]', 'output format');
 
-program.parse();
+program.parse(process.argv);
