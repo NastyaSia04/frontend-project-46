c641f424699ebd55eac796666c474c00841b34fa
NastyaSia04 <nastyasia04@gmail.com>
2024-12-26 12:11:19 +0200

    edited functions in src

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+import parse from "./parse.js";
+
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const incomingDataFormat = (filepath) => path.extname(filepath).slice(1);
+const getData = (filepath) => parse(fs.readFileSync(filepath, ('utf-8')), incomingDataFormat(filepath));
+
+const gendiff = (filepath1, filepath2) => {
+  const fullFilePath1 = getFullPath(filepath1);
+  const fullFilePath2 = getFullPath(filepath2);
+  const data1 = getData(fullFilePath1);
+  const data2 = getData(fullFilePath2);
+  console.log(data1, data2);
+  
+};
+export default gendiff;
