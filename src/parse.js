5f9d7b9ad5aa102d53e0cd2e5ab2d2682d453d05
NastyaSia04 <nastyasia04@gmail.com>
2025-01-04 18:26:10 +0200

    cancel edit

--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,13 @@
 import yaml from 'js-yaml';
 
- const parser = (data, format) => {
-  if (format === "json") {
+const parser = (data, format) => {
+  if (format === 'json') {
     return JSON.parse(data);
-  } else if (format === "yaml") {
+  } else if (format === 'yaml') {
     return yaml.load(data);
-  } else if (format === "yml") {
+  } else if (format === 'yml') {
     return yaml.load(data);
   }
- };
+};
 
- export default parser;
+export default parser;
