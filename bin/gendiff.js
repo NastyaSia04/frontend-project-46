fc8f176870a39c21f0fe6785d69caf15f9bd16f6
NastyaSia04 <nastyasia04@gmail.com>
2025-01-11 17:49:53 +0200

    added option 'stylish'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,8 +11,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    gendiff(filepath1, filepath2);
+    gendiff(filepath1, filepath2, program.opts());
   });
 program.parse(process.argv);
