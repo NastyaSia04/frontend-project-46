de4084103a8a6df05741f8f34850e87f7a5d854a
NastyaSia04 <nastyasia04@gmail.com>
2025-01-17 22:39:26 +0200

    edited options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,12 +7,14 @@ import gendiff from '../index.js';
 const program = new Command();
 
 program
+  .name('gendiff')
   .helpOption('-h, --help', 'output usage information')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    gendiff(filepath1, filepath2, program.opts());
+    const options = program.opts();
+    console.log(gendiff(filepath1, filepath2, options.format));
   });
 program.parse(process.argv);
