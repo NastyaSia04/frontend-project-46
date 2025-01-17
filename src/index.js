c19b4a4b0523686ff942ca7c97f2a10f8f4dfaf7
NastyaSia04 <nastyasia04@gmail.com>
2025-01-17 22:43:28 +0200

    edited parametres  gendiff in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import parsers from './parsers.js';
 import buildDiff from './diffBuilder.js';
-import stylish from './formatters/stylish.js';
+import getFormatName from './formatters/index.js';
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const incomingDataFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => parsers(fs.readFileSync(filepath, ('utf-8')), incomingDataFormat(filepath));
 
-const gendiff = (filepath1, filepath2, format = 'stylish') => {
+const gendiff = (filepath1, filepath2, formatName) => {
   const fullFilePath1 = getFullPath(filepath1);
   const fullFilePath2 = getFullPath(filepath2);
   const data1 = getData(fullFilePath1);
   const data2 = getData(fullFilePath2);
   const diffTree = buildDiff(data1, data2);
-  const getFormatedData = stylish(diffTree, format);
-  console.log(getFormatedData);
-  return getFormatedData;
+  const formatter = getFormatName(formatName);
+  return formatter(diffTree);
 };
 export default gendiff;
