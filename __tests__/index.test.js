037a5ff759f4c454ac134676069f49d37b5541c6
NastyaSia04 <nastyasia04@gmail.com>
2025-01-17 22:38:46 +0200

    added test plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,18 +8,31 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('diff JSON files', () => {
-  const fileJson1 = getFixturePath('file1.json');
-  const fileJson2 = getFixturePath('file2.json');
-  const receivedJsonDiff = gendiff(fileJson1, fileJson2);
-  const expectedJsonDiff = readFile('diffJsonFiles.txt');
-  expect(receivedJsonDiff).toEqual(expectedJsonDiff);
+let fileJson1;
+let fileJson2;
+let fileYml1;
+let fileYml2;
+beforeEach(() => {
+  fileJson1 = getFixturePath('file1.json');
+  fileJson2 = getFixturePath('file2.json');
+  fileYml1 = getFixturePath('file1.yml');
+  fileYml2 = getFixturePath('file2.yml');
 });
 
-test('diff YML files', () => {
-  const fileYml1 = getFixturePath('file1.yml');
-  const fileYml2 = getFixturePath('file2.yml');
-  const receivedYmlDiff = gendiff(fileYml1, fileYml2);
-  const expectedYmlDiff = readFile('diffYmlFiles.txt');
-  expect(receivedYmlDiff).toEqual(expectedYmlDiff);
+test('diff JSON files in Stylish format', () => {
+  const receivedStylishFormatJsonDiff = gendiff(fileJson1, fileJson2);
+  const expectedStylishFormatJsonDiff = readFile('expectStylishFormatJsonFiles.txt');
+  expect(receivedStylishFormatJsonDiff).toEqual(expectedStylishFormatJsonDiff);
+});
+
+test('diff YML files in Stylish format', () => {
+  const receivedStylishFormatYmlDiff = gendiff(fileYml1, fileYml2);
+  const expectedStylishFormatYmlDiff = readFile('expectStylishFormatYmlFiles.txt');
+  expect(receivedStylishFormatYmlDiff).toEqual(expectedStylishFormatYmlDiff);
+});
+
+test('diff JSON files in Plain format', () => {
+  const receivedPlainFormatDiff = gendiff(fileJson1, fileJson2, 'plain');
+  const expectedPlainFormatDiff = readFile('expectPlainFormatJsonFiles.txt');
+  expect(receivedPlainFormatDiff).toEqual(expectedPlainFormatDiff);
 });
