3a629e04344c0191f870a9b5a72b59e06c15da07
NastyaSia04 <nastyasia04@gmail.com>
2025-01-19 19:16:37 +0200

    deleted let and edited tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,44 +1,24 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
+import { describe, expect, test } from '@jest/globals';
 import gendiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let fileJson1;
-let fileJson2;
-let fileYml1;
-let fileYml2;
-beforeEach(() => {
-  fileJson1 = getFixturePath('file1.json');
-  fileJson2 = getFixturePath('file2.json');
-  fileYml1 = getFixturePath('file1.yml');
-  fileYml2 = getFixturePath('file2.yml');
-});
+const inFormat = ['json', 'yml'];
 
-test('diff JSON files in Stylish format', () => {
-  const receivedStylishFormatJsonDiff = gendiff(fileJson1, fileJson2);
-  const expectedStylishFormatJsonDiff = readFile('expectStylishFormatJsonFiles.txt');
-  expect(receivedStylishFormatJsonDiff).toEqual(expectedStylishFormatJsonDiff);
-});
-
-test('diff YML files in Stylish format', () => {
-  const receivedStylishFormatYmlDiff = gendiff(fileYml1, fileYml2);
-  const expectedStylishFormatYmlDiff = readFile('expectStylishFormatYmlFiles.txt');
-  expect(receivedStylishFormatYmlDiff).toEqual(expectedStylishFormatYmlDiff);
-});
-
-test('diff JSON files in Plain format', () => {
-  const receivedPlainFormatDiff = gendiff(fileJson1, fileJson2, 'plain');
-  const expectedPlainFormatDiff = readFile('expectPlainFormatJsonFiles.txt');
-  expect(receivedPlainFormatDiff).toEqual(expectedPlainFormatDiff);
-});
-
-test('diff JSON files in Json format', () => {
-  const receivedJsonFormatDiff = gendiff(fileJson1, fileJson2, 'json');
-  const expectedJsonFormatDiff = readFile('expectJsonFormat.txt');
-  expect(receivedJsonFormatDiff).toEqual(expectedJsonFormatDiff);
+describe('testing gendiff', () => {
+  test.each(inFormat)('testing gendiff with format', (format) => {
+    const filePath1 = getFixturePath(`file1.${format}`);
+    const filePath2 = getFixturePath(`file2.${format}`);
+    expect(gendiff(filePath1, filePath2)).toBe(readFile('expectStylishFormatFiles.txt'));
+    expect(gendiff(filePath1, filePath2, 'stylish')).toBe(readFile('expectStylishFormatFiles.txt'));
+    expect(gendiff(filePath1, filePath2, 'plain')).toBe(readFile('expectPlainFormatJsonFiles.txt'));
+    expect(gendiff(filePath1, filePath2, 'json')).toBe(readFile('expectJsonFormat.txt'));
+  });
 });
