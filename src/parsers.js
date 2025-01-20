f46c73e695b4f5cc837093fc48d90376605f5e49
NastyaSia04 <nastyasia04@gmail.com>
2025-01-20 16:33:37 +0200

    added default type

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,14 @@
 import yaml from 'js-yaml';
 
-const parsers = {
+const parse = {
   json: JSON.parse,
   yaml: yaml.load,
   yml: yaml.load,
 };
 
-export default (data, format) => parsers[format](data);
+export default (data, format) => {
+  if (!parse[format]) {
+    throw new Error(`Unknown format - ${format}! Only .json, .yaml or .yml.`);
+  }
+  return parse[format](data);
+};
